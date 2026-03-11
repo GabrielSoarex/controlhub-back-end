9c94efa865baa9b368d30dd9371c3e3805851f9f
GabrielSoarex <gabrielarantes207@gmail.com>
2026-03-10 21:25:15 -0300

    chore: initial backend structure

---
+++ src/main/java/com/control/hub/ControlhubApplication.java
@@ -0,0 +1,13 @@
+package com.control.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/control/hub/ControlhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
